2137df332b3e2f79abcaf1331d5f6711679ddaa9
Yogesh kakde <yogesh.kakde@navaantrix.com>
2024-05-31 11:18:50 +0530

    Added axios and dev env

---
+++ src/Services/axios.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  // eslint-disable-next-line no-undef
+  baseURL: process.env.REACT_APP_DOMAIN,
+});
+
+axiosInstance.interceptors.request.use(interceptor, networkError);
+function interceptor(config) {
+  config.data['Client_Id'] = localStorage.getItem('clientID');
+  config.data['Domain_Id'] = localStorage.getItem('domainID');
+  return config;
+}
+
+function networkError(error) {
+  return Promise.reject(error);
+}
+
+export default axiosInstance;
